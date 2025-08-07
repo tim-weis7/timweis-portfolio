f2759db3fbc1b97aa1a0443d096b10d3f702aa42
Tim Weis <tiwe1001@stud.hs-kl.de>
2025-08-07 15:48:03 +0200

    updated technology icons

--- src/components/TechnologyCarouselComponent.vue
+++ src/components/TechnologyCarouselComponent.vue
@@ -1,25 +1,38 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import { useDisplay } from 'vuetify';
+import html5Icon from '../assets/icons/technologies/html5.svg';
+import cssIcon from '../assets/icons/technologies/css.svg';
+import tailwindIcon from '../assets/icons/technologies/tailwindcss.svg';
+import jsIcon from '../assets/icons/technologies/javascript.svg';
+import tsIcon from '../assets/icons/technologies/typescript.svg';
+import vueIcon from '../assets/icons/technologies/vuedotjs.svg';
+import vuetifyIcon from '../assets/icons/technologies/vuetify.svg';
+import springIcon from '../assets/icons/technologies/springboot.svg';
+import nodeIcon from '../assets/icons/technologies/nodedotjs.svg';
+import postgresIcon from '../assets/icons/technologies/postgresql.svg';
+import gitIcon from '../assets/icons/technologies/git.svg';
+import githubIcon from '../assets/icons/technologies/github.svg';
+import gitkrakenIcon from '../assets/icons/technologies/gitkraken.svg';
 
 
 const display = useDisplay();
 const isMobile = computed(() => display.width.value <= 960);
 
 const technologies = [
-  { name: 'HTML5', icon: 'src/assets/icons/technologies/html5.svg' },
-  { name: 'CSS', icon: 'src/assets/icons/technologies/css.svg' },
-  { name: 'Tailwind CSS', icon: 'src/assets/icons/technologies/tailwindcss.svg' },
-  { name: 'JavaScript', icon: 'src/assets/icons/technologies/javascript.svg' },
-  { name: 'TypeScript', icon: 'src/assets/icons/technologies/typescript.svg' },
-  { name: 'Vue.js', icon: 'src/assets/icons/technologies/vuedotjs.svg' },
-  { name: 'Vuetify', icon: 'src/assets/icons/technologies/vuetify.svg' },
-  { name: 'Spring Boot', icon: 'src/assets/icons/technologies/springboot.svg' },
-  { name: 'Node.js', icon: 'src/assets/icons/technologies/nodedotjs.svg' },
-  { name: 'PostgreSQL', icon: 'src/assets/icons/technologies/postgresql.svg' },
-  { name: 'Git', icon: 'src/assets/icons/technologies/git.svg' },
-  { name: 'GitHub', icon: 'src/assets/icons/technologies/github.svg' },
-  { name: 'GitKraken', icon: 'src/assets/icons/technologies/gitkraken.svg' }
+  { name: 'HTML5', icon: html5Icon },
+  { name: 'CSS', icon: cssIcon },
+  { name: 'Tailwind CSS', icon: tailwindIcon },
+  { name: 'JavaScript', icon: jsIcon },
+  { name: 'TypeScript', icon: tsIcon },
+  { name: 'Vue.js', icon: vueIcon },
+  { name: 'Vuetify', icon: vuetifyIcon },
+  { name: 'Spring Boot', icon: springIcon },
+  { name: 'Node.js', icon: nodeIcon },
+  { name: 'PostgreSQL', icon: postgresIcon },
+  { name: 'Git', icon: gitIcon },
+  { name: 'GitHub', icon: githubIcon },
+  { name: 'GitKraken', icon: gitkrakenIcon }
 ];
 
 const repeatedTechnologies = [...technologies, ...technologies];
